efa6d1885e274e536a6edaeed0988369d236b05a
Goddchen <goddchen@gmail.com>
2024-12-20 12:41:12 +0100

    feat: tie process to stream (Android)
    Signed-off-by: Goddchen <goddchen@gmail.com>

--- flutter_native_log_handler/android/src/main/kotlin/de/goddchen/flutter/flutter_native_logs/FlutterNativeLogsPlugin.kt
+++ flutter_native_log_handler/android/src/main/kotlin/de/goddchen/flutter/flutter_native_logs/FlutterNativeLogsPlugin.kt
@@ -2,13 +2,13 @@ package de.goddchen.flutter.flutter_native_logs
 
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.EventChannel.StreamHandler
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import java.io.IOException
 import kotlin.concurrent.thread
 
 /** FlutterNativeLogsPlugin */
@@ -26,29 +26,11 @@ class FlutterNativeLogsPlugin : FlutterPlugin, MethodCallHandler, StreamHandler 
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_native_log_handler")
         channel.setMethodCallHandler(this)
         eventChannel =
-            EventChannel(flutterPluginBinding.binaryMessenger, "flutter_native_log_handler/logs")
+                EventChannel(
+                        flutterPluginBinding.binaryMessenger,
+                        "flutter_native_log_handler/logs"
+                )
         eventChannel.setStreamHandler(this)
-        // GlobalScope.launch {
-        thread(start = true) {
-            Runtime.getRuntime().exec("logcat -c")
-            logcatProcess = Runtime.getRuntime().exec("logcat")
-            logcatProcess?.let {
-                it
-                    .inputStream
-                    .bufferedReader()
-                    .useLines { lines ->
-                        lines.forEach { line ->
-                            Handler(Looper.getMainLooper()).post { eventSink?.success(line) }
-                        }
-                    }
-            }
-        }
-        // thread(start = true) {
-        //     while (true) {
-        //         Log.i("Test", "onAttachedToEngine: Test")
-        //         Thread.sleep(5 * 1000)
-        //     }
-        // }
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
@@ -66,9 +48,32 @@ class FlutterNativeLogsPlugin : FlutterPlugin, MethodCallHandler, StreamHandler 
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
         eventSink = events
+        if (logcatProcess == null) {
+            thread(start = true) {
+                Runtime.getRuntime().exec("logcat -c")
+                logcatProcess = Runtime.getRuntime().exec("logcat")
+                logcatProcess?.let {
+                    try {
+                        it.inputStream.bufferedReader().useLines { lines ->
+                            lines.forEach { line ->
+                                Handler(Looper.getMainLooper()).post { eventSink?.success(line) }
+                            }
+                        }
+                    } catch (e: IOException) {
+                        logcatProcess?.destroy()
+                        logcatProcess = null
+                    }
+                }
+                while (logcatProcess != null) {
+                    Thread.sleep(1000)
+                }
+            }
+        }
     }
 
     override fun onCancel(arguments: Any?) {
         eventSink = null
+        logcatProcess?.destroy()
+        logcatProcess = null
     }
 }
